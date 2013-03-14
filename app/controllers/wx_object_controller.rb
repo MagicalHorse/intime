c2d07b63b6b1da54958e599b90d7d76681c97be6
victor <gyyshuai@gmail.com>
2013-03-14 18:20:32 +0800

    no comment

--- app/controllers/wx_object_controller.rb
+++ app/controllers/wx_object_controller.rb
@@ -2,6 +2,7 @@ require 'digest/sha1'
 class WxObjectController < ApplicationController
   wrap_parameters :format=>:xml
   WX_TOKEN = "xhyt"
+  #function as the search get method
   def validate
     signature = params[:signature]
     encrypEcho = Digest::SHA1.hexdigest([WX_TOKEN,params[:timestamp],params[:nonce]].sort.join)
@@ -9,12 +10,13 @@ class WxObjectController < ApplicationController
     sign_result= signature if signature==encrypEcho
     render :text=>sign_result
   end
+  #function as the search post method
   def search
     mockup = {:ToUserName=>params[:xml][:FromUserName],
            :FromUserName=>params[:xml][:ToUserName],
            :CreateTime=>Time.now,
            :MsgType=>'text',
-           :Content=>'欢迎使用喜欢银泰，目前服务正在开发中，即将上线!',
+           :Content=>'Welcome to love intime, will update soon!',
            :FuncFlag=>1}
     render :xml=>mockup.to_xml(:skip_instruct=>true,:root=>'xml')
   end
