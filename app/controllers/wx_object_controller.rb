9c7223dac7adbc0ee42fb675e145d3a0600be2c3
victor <gyyshuai@gmail.com>
2013-03-14 20:08:31 +0800

    chinese message

--- app/controllers/wx_object_controller.rb
+++ app/controllers/wx_object_controller.rb
@@ -4,8 +4,10 @@ require 'net/http'
 class WxObjectController < ApplicationController
   #wrap_parameters :format=>:xml
   WX_TOKEN = "xhyt"
-  WELCOME_MSG = "喜欢银泰正在准备中，即将发布！"
-
+  WELCOME_MSG = "喜欢银泰正在准备中，即将发布!"
+  CARD_INFO_URL = "http://guide.intime.com.cn:8008/intimers/api/vipinfo/queryinfo"
+  CARD_POINT_URL = "http://guide.intime.com.cn:8008/intimers/api/vipinfo/queryscore"
+  
   def validate
     signature = params[:signature]
     encrypEcho = Digest::SHA1.hexdigest([WX_TOKEN,params[:timestamp],params[:nonce]].sort.join)
@@ -21,6 +23,23 @@ class WxObjectController < ApplicationController
            :MsgType=>'text',
            :Content=>WELCOME_MSG,
            :FuncFlag=>1}
+    render :xml=>mockup.to_xml(:skip_instruct=>true,:root=>'xml')
+  end
+  
+  def search2
+    mockup = {:ToUserName=>params[:xml][:FromUserName],
+           :FromUserName=>params[:xml][:ToUserName],
+           :CreateTime=>Time.now,
+           :MsgType=>'text',
+           :Content=>WELCOME_MSG,
+           :FuncFlag=>1}
+    in_msg_type = params[:xml][:MsgType]
+    if in_msg_type == 'text'
+      in_msg_content = params[:xml][:Content]
+      in_msg_content.parse(' ').each do |content|
+        content.is_number?
+      end
+    end
     render :xml=>mockup.to_xml(:skip_instruct=>true,:root=>'xml')
   end
 end
