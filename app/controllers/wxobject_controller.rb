1d48ffb05bed7a7d6212bbfab738a53f4da9b1da
victor <gyyshuai@gmail.com>
2013-04-05 16:30:45 +0800

    update product list

--- app/controllers/wxobject_controller.rb
+++ app/controllers/wxobject_controller.rb
@@ -273,8 +273,8 @@ class WxobjectController < ApplicationController
       resource = p['resource']
       return if resource.nil? || resource.length<1 || resource[0].name.length<1
       item = WxPicArticle.new
-      item.Title = "#{p['store']['name']}:#{p['name']}".encode(:xml=>:text)
-      item.Description = item.Title
+      item.Title = "#{p['brand']['name']}:#{p['name']}".encode(:xml=>:text)
+      item.Description = p['brand']['name']
       if first_image == true
         item.PicUrl =  large_pic_url resource[0].domain, resource[0].name
         first_image = false
