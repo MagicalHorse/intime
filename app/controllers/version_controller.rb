1a898aceb638af226a8f798282cee0524a037bed
victor <gyyshuai@gmail.com>
2013-05-23 16:11:02 +0800

    update version

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -4,7 +4,11 @@ class VersionController < ApplicationController
     return render :json=> error_500 {'version param is empty!'} if version_in.nil?
     version_int = Version.version_fromstring(version_in.to_s)
     latest_version = Version.where('no>? and status=?',version_int,1).order('no desc').first
-    return render :json=>succ_200 {t(:vc_islatest)} if latest_version.nil?
+    return render :json=>succ_compose_200 {|h|
+      h[:data]={
+        :title=> t(:vc_islatest),
+        :type=> 0
+      }} if latest_version.nil?
     title = t(:vc_newupdatemsg) unless latest_version.nil?
     return render :json=>succ_compose_200 {|h|
       h[:data]= {
