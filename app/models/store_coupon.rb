3805e7a74656806322e13ecb7e6fde586c85815d
victor <gyyshuai@gmail.com>
2013-06-23 16:52:26 +0800

    fix sqs message bug

--- app/models/store_coupon.rb
+++ app/models/store_coupon.rb
@@ -4,24 +4,14 @@ class StoreCoupon < ActiveRecord::Base
   class<<self
     def sync_one(msg,type)
       return if msg.nil?
-      if !(msg[:lastupdate].nil?)
-        coupon_old = self.find_by_code(msg[:code])
-        logger.info 'enter coupon old'
-        logger.info msg[:lastupdate]
-        if !(coupon_old.nil?) && coupon_old.updated_at>=msg[:lastupdate]
-          return
-        end
-      end
-      exist_coupon = self.find_or_initialize_by_code(msg[:code])
-      exist_coupon.status= msg[:status]
-      exist_coupon.code =msg[:code]
-      exist_coupon.amount= msg[:amount]
-      exist_coupon.userid= msg[:userid]
-      exist_coupon.validstartdate=msg[:validstartdate]
-      exist_coupon.validenddate=msg[:validenddate]
-      exist_coupon.vipcard= msg[:vipcard]
-      exist_coupon.coupontype=type
-      exist_coupon.save
+      coupon_old = self.find_by_code(msg[:code])
+      return unless coupon_old.nil?
+      self.create(:code=>msg[:code],:status=>msg[:status],:amount=>msg[:amount],:userid=>msg[:userid] \
+              ,:validstartdate=>msg[:validstartdate] \
+              ,:validenddate=>msg[:validenddate] \
+              ,:vipcard=>msg[:vipcard] \
+              ,:coupontype=>type)
+      
     end
   end
 end
