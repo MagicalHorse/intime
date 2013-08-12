f62dffcd10a2c8b15e4a383753207defdf795acc
victor <gyyshuai@gmail.com>
2013-08-12 13:47:03 +0800

    add channel param

--- config/initializers/api_intime.rb
+++ config/initializers/api_intime.rb
@@ -5,6 +5,6 @@ elsif Rails.env.stage?
   
 else
   API_HOST ="http://apis.youhuiin.com/api"
-  CLIENT_VERSION = '2.1.2'
-  API_KEY = 'yintai123456'
+  CLIENT_VERSION = '2.3'
+  API_KEY = 'yintai'
 end
