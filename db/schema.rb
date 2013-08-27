873e54a518bf43638e1d73abf2cc1f894efc822b
Kimi <kimigao1986@gmail.com>
2013-08-27 23:38:20 +0800

    change db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131025014638) do
+
+ActiveRecord::Schema.define(:version => 20130624013326) do
+
 
   create_table "auth_keys", :force => true do |t|
     t.string   "private"
