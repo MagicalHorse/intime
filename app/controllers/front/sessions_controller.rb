241a4d0dc86bbcb9cbb8760cc6c17ef1adb5291c
tumayun <tumayun.2010@gmail.com>
2013-09-15 00:03:04 +0800

    加上腾讯微博 oauth 登录，修改 current_user

--- app/controllers/front/sessions_controller.rb
+++ app/controllers/front/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Front::SessionsController < Front::BaseController
+
   def create
     login_user = login_from_api
     if login_user[:isSuccessful]==true
@@ -8,15 +9,28 @@ class Front::SessionsController < Front::BaseController
       set_anonymous_user
     end
   end
-  private 
+
+  private
+
   def login_from_api
     auth_data = request.env['omniauth.auth']
-    login_type =0
-    if auth_data.provider=='weibo'
-      login_type=1
-    elsif auth_data.provider=='qq_connect'
-      login_type=3
-    end
-    API::LoginRequest.post(request,{:outsiteuid=>auth_data.uid,:outsitenickname=>auth_data.info.nickname,:outsitetype=>login_type})
+    login_type = case auth_data.provider
+                 when 'weibo'      then Settings.provider.weibo
+                 when 'tqq'        then Settings.provider.tqq
+                 when 'qq_connect' then Settings.provider.qq_connect
+                 else Settings.provider.other
+                 end
+
+    user_hash = API::LoginRequest.post(request, {
+      :outsiteuid       => auth_data.uid,
+      :outsitenickname  => auth_data.info.nickname,
+      :outsitetype      => login_type
+    })
+
+    user_hash[:data] ||= {}
+    user_hash[:data][:access_token] = auth_data.credentials.token
+    # 腾讯微博的 access_token 有效期为 3 个月  http://wiki.open.t.qq.com/index.php/OAuth2.0%E9%89%B4%E6%9D%83
+    user_hash[:data][:expires_at]   = login_type == Settings.provider.tqq ? 3.months.since.to_i : auth_data.credentials.expires_at
+    user_hash
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :weibo, '1136922935', 'cee745ae09ff0000428e0a52757ff175'
   provider :qq_connect, '100382932','8acc22a900a6cf3a144c4e7364dafa78'
+  provider :tqq, '801302732', 'cd497771f88f6971ad11855088d050fd'
 end
