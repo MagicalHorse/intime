b61e6522cfa461c7ce6175c0d8b89c2a5af3d489
hanqing <hanqing@rd.tuan800.com>
2013-10-14 23:25:23 +0800

    未登录 无法访问我的银泰页面

--- app/controllers/front/users_center_controller.rb
+++ app/controllers/front/users_center_controller.rb
@@ -1,2 +1,13 @@
 class Front::UsersCenterController < Front::BaseController 
+
+  before_filter :check_current_user, :only => [:my_favorite, :my_share, :my_promotion]
+
+  protected
+
+  def check_current_user
+    if  current_user.blank?
+      redirect_to login_path
+    end
+  end
+
 end
