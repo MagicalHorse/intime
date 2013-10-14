9e272da64995a77ca6e92e29b341831b24044bc4
Kimi <kimigao1986@gmail.com>
2013-10-15 01:38:07 +0800

    add product page

---
+++ app/controllers/front/comments_controller.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+class Front::ProfileController < Front::BaseController
+  before_filter :authenticate! only: [:create]
+
+  def index
+  end
+
+  def create
+    @comment = API::Comment.create(request, params[:comment].merge(replyuser: current_user.id))
+
+    respond_to do |format|
+      format.js
+    end
+  end
+end
