fa71e07a2b4a40d3e617e30d6f0e88d12a57b7f6
victor <gyyshuai@gmail.com>
2013-10-16 12:12:04 +0800

    add omniauth-wechat

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :weibo, Settings.omniauth.weibo.key, Settings.omniauth.weibo.secret
   provider :qq_connect, Settings.omniauth.qq_connect.key, Settings.omniauth.qq_connect.secret
   provider :tqq2, Settings.omniauth.tqq.key, Settings.omniauth.tqq.secret
+  provider :wechat, Settings.omniauth.wechat.key, Settings.omniauth.wechat.secret
 end
