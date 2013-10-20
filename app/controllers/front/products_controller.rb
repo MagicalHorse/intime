1640465f940e73eac8cb5dab4fb66ace3712cef0
hanqing <hanqing@rd.tuan800.com>
2013-10-20 10:50:55 +0800

    我的资料 他的资料

--- app/controllers/front/products_controller.rb
+++ app/controllers/front/products_controller.rb
@@ -17,7 +17,7 @@ class Front::ProductsController < Front::BaseController
     }
     result  = Stage::Product.search(options)
     results = result["data"].slice("pageindex", "pagesize", "totalcount", "totalpaged")
-    results.merge! (gen_data(result["data"]["products"]))
+    results.merge! (gen_search_data(result["data"]["products"]))
     render :json =>results.to_json , callback: params[:callback]
   end
 
@@ -74,6 +74,24 @@ class Front::ProductsController < Front::BaseController
     options
   end
 
+  def gen_search_data(datas)
+    results = {}
+    items = []
+    datas && datas.each do |item|
+      image_info = item["resources"].first
+      items << {
+        title:     item["name"],
+        price:     item["price"],
+        originalPrice:  item["unitprice"],
+        likeCount: item["likecount"],
+        url:       front_product_path(item["id"]),
+        imageUrl:  image_info.blank? ? "" : middle_pic_url(image_info)
+      }
+    end
+    results[:datas] = items
+    results
+  end
+
   def gen_share(datas)
     results = {}
     items = []
@@ -84,7 +102,7 @@ class Front::ProductsController < Front::BaseController
         price:     item["price"],
         originalPrice:  item["originprice"],
         likeCount: item["likecount"],
-        url:       "",
+        url:       front_product_path(item["id"]),
         imageUrl:  image_info.blank? ? "" : middle_pic_url(image_info)
       }
     end
@@ -102,7 +120,7 @@ class Front::ProductsController < Front::BaseController
         price:     item["price"],
         originalPrice:  item["price"],
         likeCount: item["favorable"],
-        url:       "",
+        url:       front_product_path(item["id"]),
         imageUrl:  image_info.blank? ? "" : middle_pic_url(image_info)
       }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -153,14 +153,15 @@ IntimeService::Application.routes.draw do
     # Directs /admin/products/* to Admin::ProductsController
     #     # (app/controllers/admin/products_controller.rb)
 
-    get '/hotwords',       to: 'common#hotwords'
-
-    get '/my_favorite',  to: 'users_center#my_favorite'
-    get '/my_share',     to: 'users_center#my_share'
-    get '/my_promotion', to: 'users_center#my_promotion'
-    get '/follows',      to: 'users_center#follows'
-    get '/fans',         to: 'users_center#fans'
-    get '/follow',       to: 'users_center#follow'
+    get '/hotwords',      to: 'common#hotwords'
+    get '/my_favorite',   to: 'users_center#my_favorite'
+    get '/my_share',      to: 'users_center#my_share'
+    get '/my_promotion',  to: 'users_center#my_promotion'
+    get '/follows',       to: 'users_center#follows'
+    get '/fans',          to: 'users_center#fans'
+    get '/follow',        to: 'users_center#follow'
+    get '/profile',       to: 'users_center#profile'
+    get '/his_info/:userid',       to: 'users_center#his_info'
 
     resources :products, :only=>[:index, :show] do
       collection do
