c49c345f07c74e0bcf2c340cbde41d78999281d5
tumayun <tumayun.2010@gmail.com>
2013-10-23 23:57:12 +0800

    个人资料修改

--- app/controllers/front/profile_controller.rb
+++ app/controllers/front/profile_controller.rb
@@ -4,4 +4,20 @@ class Front::ProfileController < Front::BaseController
 
   def index
   end
+
+  def edit
+    @user = API::Customer.show(request)[:data].slice(:desc, :nickname, :logo, :gender, :mobile)
+  end
+
+  def update
+    result = API::Customer.update(request, params[:user].except(:logo))
+
+    if false#result[:isSuccessful]
+      redirect_to front_my_profile_path, notice: '更新成功。'
+    else
+      @user = params[:user]
+      flash.alert = result[:message]
+      render :edit
+    end
+  end
 end
