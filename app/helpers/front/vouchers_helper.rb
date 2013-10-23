9b9d6fbf070080a852b7846c5d04140a4fbad029
Kimi <kimigao1986@gmail.com>
2013-10-23 22:45:39 +0800

    修复绑定会员卡展示错误的问题

--- app/helpers/front/vouchers_helper.rb
+++ app/helpers/front/vouchers_helper.rb
@@ -1,6 +1,6 @@
 module Front::VouchersHelper
   def fetch_user_card_info
-    return nil unless current_user.isbindcard && current_user.cardno
+    return nil unless current_user.isbindcard
 
     card_info = API::Card.detail(request, cardno: current_user.cardno)['data']
   end
