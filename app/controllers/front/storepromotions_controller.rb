a3d0dbe8a471d29df1522cc078b3b08927278fc8
Kimi <kimigao1986@gmail.com>
2013-10-25 01:21:46 +0800

    完善积点兑换、添加关于和反馈页面

--- app/controllers/front/storepromotions_controller.rb
+++ app/controllers/front/storepromotions_controller.rb
@@ -8,7 +8,17 @@ class Front::StorepromotionsController < Front::BaseController
     @storepromotion = Stage::Storepromotion.fetch(params[:id])
   end
 
+  def amount
+    amount = API::Storepromotion.amount(request, storepromotionid: params[:id], points: params[:points])[:data]
+
+    render json: {amount: amount[:amount]}
+  end
+
   def exchange
-    
+    @result = API::Storepromotion.exchange(request, params.slice(:points, :identityno, :storeid).merge(storepromotionid: params[:id]))
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
