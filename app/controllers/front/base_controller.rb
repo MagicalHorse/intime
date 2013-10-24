4eefe8ceaaf513c915a5317d41e17bcd71b6605f
Kimi <kimigao1986@gmail.com>
2013-10-24 18:21:31 +0800

    add skip_before_filter to front/base_controller

--- app/controllers/front/base_controller.rb
+++ app/controllers/front/base_controller.rb
@@ -4,6 +4,10 @@ class Front::BaseController < ApplicationController
   helper_method :current_user, :signed_in?
   before_filter :update_current_user
 
+  # TODO
+  # 前端测试用，暂时加上
+  skip_before_filter :verify_authenticity_token
+
   def current_user
     @current_user ||= session[:current_user]
   end
