8953d329215b6c03ab59c748c92b2bcec11075b2
Kimi <kimigao1986@gmail.com>
2013-10-25 23:57:16 +0800

    完善创建反馈

--- app/controllers/front/about_controller.rb
+++ app/controllers/front/about_controller.rb
@@ -8,6 +8,9 @@ class Front::AboutController < Front::BaseController
   end
 
   def create_feedback
+    @result = API::Feedback.create(request, params.slice(:content, :cantact))
+
+    respond_to { |format| format.js }
   end
 
 end
