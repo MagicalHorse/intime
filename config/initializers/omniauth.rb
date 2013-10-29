8a42c06848551ce400b9bdbc7727a5b87f210d7d
tumayun <tumayun.2010@gmail.com>
2013-10-30 00:49:29 +0800

    修复测试反馈提的bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :tqq2, Settings.omniauth.tqq.key, Settings.omniauth.tqq.secret
   provider :wechat, Settings.omniauth.wechat.key, Settings.omniauth.wechat.secret
 end
+
+if Rails.env.production?
+  OmniAuth.config.on_failure = Proc.new do |env|
+    [302, {'Location' => '/', 'Content-Type'=> 'text/html'}, []]
+  end
+end
