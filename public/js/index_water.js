2fa5025cafcb8ea0e9ebe1373afda18eb5f1ae6d
Kimi <kimigao1986@gmail.com>
2013-11-12 22:44:03 +0800

    更新首页js

--- public/js/index_water.js
+++ public/js/index_water.js
@@ -18,123 +18,120 @@ $('#banner').royalSlider({
 	},
 	transitionType:'move',//切换过渡类型	‘move’ 或 ‘fade’
 });
+function clears(){
+	page = 1;
+	$('#tiles').empty();
+	//$('#tiles').masonry('reload');
+	$('#tiles').masonry('reloadItems');
+	}
 
+       var handler = null,
+          page = 1,
+          _isLoadingMore = false,
+          apiURL = 'http://stage.youhuiin.com/front/promotions/get_list.json';
 
-   var handler = null;
-    var page = 1;
-    var isLoading = false;
-    var apiURL = 'http://stage.youhuiin.com/front/promotions/get_list.json'
-    
-    // Prepare layout options.
-    var options = {
-      autoResize: true, // This will auto-update the layout when the browser window is resized.
-      container: $('#tiles'), // Optional, used for some extra CSS styling
-      offset: 2, // Optional, the distance between grid items
-      itemWidth: 210 // Optional, the width of a grid item
-    };
-    
-    /**
-     * When scrolled all the way to the bottom, add more tiles.
-     */
-    function onScroll(event) {
-      // Only check when we're not still waiting for data.
-      if(!isLoading) {
-        // Check if we're within 100 pixels of the bottom edge of the broser window.
-        var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
-        if(closeToBottom) {
-          loadData(sort);
+      // Prepare layout options.
+     var scrollContainer = $('#tiles').masonry();
+	 var msnry = scrollContainer.data('masonry');
+
+      function onScroll(event) {
+        // Only check when we're not still waiting for data.
+        if(!_isLoadingMore) {
+          // Check if we're within 100 pixels of the bottom edge of the broser window.
+          var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
+          if(closeToBottom) {
+            //loadData(sort);
+			loadData(sort);
+          }
         }
-      }
-    };
-    
-    /**
-     * Refreshes the layout.
-     */
-    function applyLayout() {
-      // Clear our previous layout handler.
-      if(handler) handler.wookmarkClear();
-      
-      // Create a new layout handler.
-      handler = $('#tiles li');
-      handler.wookmark(options);
-    };
-    
-    /**
-     * Loads data from the API.
-     */
-    function loadData($type) {
-      isLoading = true;
-      $('#loaderCircle').show();
-      sort = $type;
-      $.ajax({
-        url: apiURL,
-        dataType: 'jsonp',
-        data: {page: page,sort:sort}, // Page parameter to make sure we load new data
-        success: onLoadData
-      });
-    };
-    
-    /**
-     * Receives data from the API, creates HTML for images and updates the layout
-     */
-    function onLoadData(data) {
-      isLoading = false;
-      $('#loaderCircle').hide();
-      
-      // Increment page index for future calls.
-      page++;
-      
-      // Create HTML for the images.
-      var html = '';
-      var i=0, length=data.datas.length;
-      for(; i<length; i++) {
-        if(sort==1){
-			html+='<li>';
+      };
+
+      function loadData($sort) {
+        _isLoadingMore = true;
+        $('#loaderCircle').show();
+		sort = $sort;
+		//alert(sort);
+        $.ajax({
+          url: apiURL,
+          dataType: 'jsonp',
+          data: {page: page,sort:sort}, // Page parameter to make sure we load new data
+          success: onLoadData
+        });
+      };
+
+      /**
+       * Receives data from the API, creates HTML for images and updates the layout
+       */
+      function onLoadData(data) {
+        _isLoadingMore = false;
+        $('#loaderCircle').hide();
+
+        // Increment page index for future calls.
+        page++;
+
+          var i=0, length=data.datas.length;
+			var html = '';
+			if (length<=0)
+			    html = '<p style="text-align:center;font-size: 16px;line-height:30px;">更多精彩活动敬请期待！</p>';
+				$('#tiles').after(html);
+				//alert(length);
+			var elems = [];
+			var fragment = document.createDocumentFragment();
+
+
+			  for ( ; i < length; i++ ) {
+
+ if(sort==1){
+			html+='<li class="scrollItem">';
 							html+='<div class="thumbnail">';
-								html+='<h3><i class="icon_title"></i><a href="'+data.datas[i].url+'" title="">'+data.datas[i].title+'</a></h3>';
-								html+='<div class="action"> <a href="'+data.datas[i].url+'"><img src="'+data.datas[i].imageUrl+'" alt=" "></a>';
+								html+='<h3><i class="icon_title"></i><a href="promo.html" title="">'+data.datas[i].title+'</a></h3>';
+								html+='<div class="action"> <a href="promo.html"><img src="'+data.datas[i].imageUrl+'" alt=" "></a>';
 									html+='<p>喜欢银泰，乐享三倍积点。银泰年中庆，小积点也能玩出大动作，三倍积点大赠送啦！</p>';
 								html+='</div>';
 								html+='<h3 class="time bottom">活动时间：<span>'+data.datas[i].startDate+'-'+data.datas[i].endDate+'</span></h3>';
-								html+='<small> <span class="pull-left"><a href="'+data.datas[i].storeUrl+'"><i class="icon-map-marker"></i>'+data.datas[i].storeName+'</a></span> <span class="pull-right"><i class="icon-heart"></i>'+data.datas[i].likeCount+'+</span> </small> </div>';
+								html+='<small> <span class="pull-left"><a href="shop.html"><i class="icon-map-marker"></i>'+data.datas[i].storeName+'</a></span> <span class="pull-right"><i class="icon-heart"></i>'+data.datas[i].likeCount+'+</span> </small> </div>';
 						html+='</li>';
 		}else if(sort==2) {
-			html+='<li>';
+			html+='<li class="scrollItem">';
 							html+='<div class="thumbnail">';
 								html+='<h3 class="time">活动时间：<span>'+data.datas[i].startDate+'-'+data.datas[i].endDate+'</span></h3>';
-								html+='<div class="action"> <a href="'+data.datas[i].url+'"><img src="'+data.datas[i].imageUrl+'" alt=" "></a>';
+								html+='<div class="action"> <a href="#"><img src="'+data.datas[i].imageUrl+'" alt=" "></a>';
 									html+='<p>喜欢银泰，乐享三倍积点。银泰年中庆，小积点也能玩出大动作，三倍积点大赠送啦！</p>';
 								html+='</div>';
-								html+='<h3 class="bottom"><i class="icon_title"></i><a href="'+data.datas[i].url+'" title="">'+data.datas[i].title+'</a></h3>';
-								html+='<small> <span class="pull-left"><a href="'+data.datas[i].storeUrl+'"><i class="icon-map-marker"></i>'+data.datas[i].storeName+'</a></span> <span class="pull-right"><i class="icon-heart"></i>'+data.datas[i].likeCount+'+</span> </small> </div>';
+								html+='<h3 class="bottom"><i class="icon_title"></i><a href="#" title="">'+data.datas[i].title+'</a></h3>';
+								html+='<small> <span class="pull-left"><a href="shop.html"><i class="icon-map-marker"></i>'+data.datas[i].storeName+'</a></span> <span class="pull-right"><i class="icon-heart"></i>'+data.datas[i].likeCount+'+</span> </small> </div>';
 						html+='</li>';
 		} else if(sort==3) {
-			html+='<li>';
+			html+='<li class="scrollItem">';
 							html+='<div class="thumbnail">';
-								html+='<h3><i class="icon_title"></i><a href="'+data.datas[i].url+'" title="">'+data.datas[i].title+'</a></h3>';
-								html+='<div class="action"> <a href="'+data.datas[i].url+'"><img src="'+data.datas[i].imageUrl+'" alt=" "></a>';
+								html+='<h3><i class="icon_title"></i><a href="promo.html" title="">'+data.datas[i].title+'</a></h3>';
+								html+='<div class="action"> <a href="promo.html"><img src="'+data.datas[i].imageUrl+'" alt=" "></a>';
 									html+='<p>喜欢银泰，乐享三倍积点。银泰年中庆，小积点也能玩出大动作，三倍积点大赠送啦！</p>';
 								html+='</div>';
 								html+='<h3 class="time bottom">活动时间：<span>'+data.datas[i].startDate+'-'+data.datas[i].endDate+'</span></h3>';
-								html+='<small> <span class="pull-left"><a href="'+data.datas[i].storeUrl+'"><i class="icon-map-marker"></i>'+data.datas[i].storeName+'</a></span> <span class="pull-right"><i class="icon-heart"></i>'+data.datas[i].likeCount+'+</span> </small> </div>';
+								html+='<small> <span class="pull-left"><a href="shop.html"><i class="icon-map-marker"></i>'+data.datas[i].storeName+'</a></span> <span class="pull-right"><i class="icon-heart"></i>'+data.datas[i].likeCount+'+</span> </small> </div>';
 						html+='</li>';
 			}
-      }
-      
-      // Add image HTML to the page.
-      $('#tiles_lsit').append(html);
-      
-      // Apply layout.
-      applyLayout();
-    };
-    function clears() {
-		page =1;
-		$('#tiles_lsit').empty();
-		}
-    $(document).ready(new function() {
+				var elem = $(html).get(i);
+				fragment.appendChild(elem);
+				elems.push( elem );
+			  }
+			
+				// Start Masonry
+				scrollContainer.imagesLoaded( function(){
+					scrollContainer.masonry({
+						itemSelector : '.scrollItem'
+					});
+				});
+
+				scrollContainer.append(fragment);
+				msnry.appended(elems);
+      };
+	   
+       $(document).ready(new function() {
       // Capture scroll event.
       $(document).bind('scroll', onScroll);
-      
+
       // Load first data from the API.
       loadData('3');
-    });
+									  });
