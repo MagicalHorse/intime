7c717df4f18e89893d549307df5ec1aeedd604fe
hanqing <hanqing@rd.tuan800.com>
2013-11-15 09:02:04 +0800

    我喜欢的宝贝

---
+++ public/js/environment.js
@@ -0,0 +1,8 @@
+window.intime = window.intime || {};
+intime = window.intime;
+$.extend(intime,{
+	env:{
+		host: 'http://stage.youhuiin.com/',
+		pagesize: 10
+	}
+});
--- public/js/space.js
+++ public/js/space.js
@@ -1,113 +1,117 @@
-function clears(){
-	page = 1;
-	$('#tiles').empty();
-	//$('#tiles').masonry('reload');
-	$('#tiles').masonry('reloadItems');
-	}
+window.intime = window.intime || {};
+intime = window.intime;
+$.extend(intime, {
+	index: {
+		_page: 1,
+		_sort: '',
+		_searchpath: 'front/products/search_api.json',
+		_listpath: 'front/products/my_favorite_api.json',
+		_container: $('#tiles'),
+		_msnry: null,
+		_isMsnryInit: false,
+		_isLoadingMore: false,
 
-var handler = null,
-          page = 1,
-          _isLoadingMore = false,
-          apiURL = 'http://stage.youhuiin.com/front/products/my_favorite_api.json';
+		onLoad: function(data) {
+			var _this = intime.index;
+			_this._page++;
 
-      // Prepare layout options.
-     var scrollContainer = $('#tiles').masonry();
-	 var msnry = scrollContainer.data('masonry');
-
-      function onScroll(event) {
-        // Only check when we're not still waiting for data.
-        if(!_isLoadingMore) {
-          // Check if we're within 100 pixels of the bottom edge of the broser window.
-          var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
-          if(closeToBottom) {
-            //loadData(sort);
-			loadData(loveType);
-          }
-        }
-      };
-
-      function loadData($loveType) {
-        _isLoadingMore = true;
-        $('#loaderCircle').show();
-		loveType = $loveType;
-		//alert(sort);
-        $.ajax({
-          url: apiURL,
-          dataType: 'jsonp',
-          data: {page: page,loveType:loveType}, // Page parameter to make sure we load new data
-          success: onLoadData
-        });
-      };
-
-      /**
-       * Receives data from the API, creates HTML for images and updates the layout
-       */
-      function onLoadData(data) {
-        _isLoadingMore = false;
-        $('#loaderCircle').hide();
-
-        // Increment page index for future calls.
-        page++;
-
-          var i=0, length=data.datas.length;
-			var html = '';
-			if (length<=0)
-				return;
-				//alert(length);
+			var length = data.datas.length;
+			if (length <= 0) return;
 			var elems = [];
 			var fragment = document.createDocumentFragment();
-
-
-			  for ( ; i < length; i++ ) {
-				if(loveType==1){
-							html+='<li class="scrollItem">';
-										html+='<div class="thumbnail">';
-											html+='<div class="action">';
-												html+='<a href="'+data.datas[i].url+'"><img src="'+data.datas[i].imageUrl+'" alt="'+data.datas[i].imageUrl+'"></a>';
-												html+='<span class="like"><i class="icon-heart icon-white"></i>'+data.datas[i].likeCount+'+</span>';
-											html+='</div>';
-											html+='<h4><a href="'+data.datas[i].url+'">'+data.datas[i].title+'</a></h4>';
-											html+='<small><span class="pull-left num">吊牌价：<em>￥'+data.datas[i].originalPrice+'</em></span><span class="pull-right price">销售价：<em>￥'+data.datas[i].price+'</em></span></small>';
-										html+='</div>';
-									html+='</li>';
-						}else if(loveType==2) {
-							html+='<li class="scrollItem">';
-										 html+='<div class="thumbnail">';
-											 html+='<div class="action"> <a href="'+data.datas[i].url+'" title="'+data.datas[i].title+'"><img src="'+data.datas[i].imageUrl+'" alt="'+data.datas[i].title+'"></a> </div>';
-										 html+='</div>';
-									 html+='</li>';
-						} else if(loveType==3) {
-							html+='<li class="scrollItem">';
-										html+='<div class="thumbnail">';
-											html+='<div class="action">';
-												html+='<a href="'+data.datas[i].url+'"><img src="'+data.datas[i].imageUrl+'" alt="'+data.datas[i].imageUrl+'"></a>';
-												html+='<span class="like"><i class="icon-heart icon-white"></i>'+data.datas[i].likeCount+'+</span>';
-											html+='</div>';
-											html+='<h4><a href="'+data.datas[i].url+'">'+data.datas[i].title+'</a></h4>';
-											html+='<small><span class="pull-left num">吊牌价：<em>￥'+data.datas[i].originalPrice+'</em></span><span class="pull-right price">销售价：<em>￥'+data.datas[i].price+'</em></span></small>';
-										html+='</div>';
-									html+='</li>';
-							}
-				var elem = $(html).get(i);
+			$(data.datas).each(function() {
+				var html = '';
+				var one = this;
+				if (loveType == 1) {
+					html += '<li class="scrollItem">';
+					html += '<div class="thumbnail">';
+					html += '<div class="action">';
+					html += '<a href="product.html"><img src="' + one.imageUrl + '" alt="' + one.imageUrl + '"></a>';
+					html += '<span class="like"><i class="icon-heart icon-white"></i>' + one.likeCount + '+</span>';
+					html += '</div>';
+					html += '<h4><a href="product.html">' + one.title + '</a></h4>';
+					html += '<small><span class="pull-left num">吊牌价：<em>￥' + one.originalPrice + '</em></span><span class="pull-right price">销售价：<em>￥' + one.price + '</em></span></small>';
+					html += '</div>';
+					html += '</li>';
+				} else if (loveType == 2) {
+					html += '<li class="scrollItem">';
+					html += '<div class="thumbnail">';
+					html += '<div class="action"> <a href="promo.html" title="' + one.title + '"><img src="' + one.imageUrl + '" alt="' + one.title + '"></a> </div>';
+					html += '</div>';
+					html += '</li>';
+				} else if (loveType == 3) {
+					html += '<li class="scrollItem">';
+					html += '<div class="thumbnail">';
+					html += '<div class="action">';
+					html += '<a href="product.html"><img src="' + one.imageUrl + '" alt="' + one.imageUrl + '"></a>';
+					html += '<span class="like"><i class="icon-heart icon-white"></i>' + one.likeCount + '+</span>';
+					html += '</div>';
+					html += '<h4><a href="product.html">' + one.title + '</a></h4>';
+					html += '<small><span class="pull-left num">吊牌价：<em>￥' + one.originalPrice + '</em></span><span class="pull-right price">销售价：<em>￥' + one.price + '</em></span></small>';
+					html += '</div>';
+					html += '</li>';
+				}
+				var elem = $(html).get(0);
 				fragment.appendChild(elem);
-				elems.push( elem );
-			  }
-			
-				// Start Masonry
-				scrollContainer.imagesLoaded( function(){
-					scrollContainer.masonry({
-						itemSelector : '.scrollItem'
+				elems.push(elem);
+			});
+			_this._container.append(fragment);
+			_this._container.imagesLoaded(function() {
+				if (!_this._isMsnryInit) {
+					_this._isMsnryInit = true;
+					_this._msnry = new Masonry(_this._container[0], {
+						itemSelector: '.scrollItem'
 					});
-				});
+				} else {
+					_this._msnry.appended(elems);
+				}
+			});
+		},
+		loadData: function($loveType) {
+			this._isLoadingMore = true;
+			$('#loaderCircle').show();
+			var _this = this;
+			loveType = $loveType;
+			$.ajax({
+				url: this.listUrl(),
+				dataType: 'jsonp',
+				data: {
+					page: this._page,
+					loveType: loveType
+				},
+				success: this.onLoad
+			}).always(function() {
+				_this._isLoadingMore = false;
+				$('#loaderCircle').hide();
+			});
 
-				scrollContainer.append(fragment);
-				msnry.appended(elems);
-      };
-	   
-       $(document).ready(new function() {
-      // Capture scroll event.
-      $(document).bind('scroll', onScroll);
+		},
+		clears: function() {
 
-      // Load first data from the API.
-      loadData('2');
-									  });
+			this._page = 1;
+			this._container.empty();
+			if (this._msnry) {
+				var items = this._msnry.getItemElements();
+				if (items && items.length > 0) {
+					this._msnry.remove(items);
+					this._isMsnryInit = false;
+					this._msnry.destroy();
+				}
+			}
+		},
+		onScroll: function(event) {
+			// Only check when we're not still waiting for data.
+			var _this = intime.index;
+			if (!_this._isLoadingMore) {
+				// Check if we're within 100 pixels of the bottom edge of the broser window.
+				var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
+				if (closeToBottom) {
+					_this.loadData(loveType);
+				}
+			}
+		},
+		listUrl: function() {
+			return intime.env.host + this._listpath;
+		}
+	}
+});
