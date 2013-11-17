e9ab8c0676fa9bdbf839a975c04f85ae001bbbd4
hanqing <hanqing@rd.tuan800.com>
2013-11-17 23:14:46 +0800

    页面和js重新调整

--- public/js/sort_list.js
+++ public/js/sort_list.js
@@ -1,102 +1,107 @@
-var handler = null,
-          page = 1,
-          _isLoadingMore = false,
-          apiURL = 'http://stage.youhuiin.com/front/products/list_api.json';
+window.intime = window.intime || {};
+intime = window.intime;
+$.extend(intime, {
+	index: {
+		_page: 1,
+		_sort: '',
+		_searchpath: 'front/products/search_api.json',
+		_listpath: 'front/products/list_api.json',
+		_container: $('#tiles'),
+		_msnry: null,
+		_isMsnryInit: false,
+		_isLoadingMore: false,
 
-      // Prepare layout options.
-     var scrollContainer = $('#tiles').masonry();
-	 var msnry = scrollContainer.data('masonry');
+		onLoad: function(data) {
+			var _this = intime.index;
+			_this._page++;
 
-      function onScroll(event) {
-        // Only check when we're not still waiting for data.
-        if(!_isLoadingMore) {
-          // Check if we're within 100 pixels of the bottom edge of the broser window.
-          var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
-          if(closeToBottom) {
-            //loadData(sort);
-			loadData(type,entity_id);
-          }
-        }
-      };
-
-      function loadData($type,$entity_id) {
-        _isLoadingMore = true;
-        $('#loaderCircle').show();
-		storeid = $type;
-		value = $entity_id;
-					 $.ajax({
-					  url: apiURL,
-					  dataType: 'jsonp',
-					  data: {page: page,type:type,entity_id:entity_id}, // Page parameter to make sure we load new data
-					  success: onLoadData
-				     });	
-		
-      };
-
-      /**
-       * Receives data from the API, creates HTML for images and updates the layout
-       */
-      function onLoadData(data) {
-        _isLoadingMore = false;
-        $('#loaderCircle').hide();
-
-        // Increment page index for future calls.
-        page++;
-
-          var i=0, length=data.datas.length;
-			var html = '';
-			if (length<=0)
-				return;
-				//alert(length);
+			var length = data.datas.length;
+			if (length <= 0) return;
 			var elems = [];
 			var fragment = document.createDocumentFragment();
-
-              
-			  for ( ; i < length; i++ ) {
-				  //alert(data.datas[i].title);
-			    html+='<li class="scrollItem">';
-						html+='<div class="thumbnail">';
-							html+='<div class="action">';
-								html+='<!--优惠-->';
-								if(data.datas[i].flag.toString()=="true"){
-									html+='<span class="discount">优惠</span>';
-									html+='<span class="triangle"></span>';
-									} 
-								html+='<!--优惠-->';
-								html+='<a href="'+data.datas[i].url+'"><img src="'+data.datas[i].imageUrl+'" alt="'+data.datas[i].title+'"></a>';
-								html+='<span class="like"><i class="icon-heart icon-white"></i>'+data.datas[i].likeCount+'+</span>';
-							html+='</div>';
-							html+='<h4><a href="'+data.datas[i].url+'" title="">'+data.datas[i].title+'</a></h4>';
-							html+='<small><span class="pull-left num">吊牌价：<em>￥'+data.datas[i].originalPrice+'</em></span><span class="pull-right price">销售价：<em>￥'+data.datas[i].price+'</em></span></small>';
-						html+='</div>';
-					html+='</li>';
-				var elem = $(html).get(i);
+			$(data.datas).each(function() {
+				var html = '';
+				var one = this;
+				html+='<li class="scrollItem">';
+				html+='<div class="thumbnail">';
+				html+='<div class="action">';
+				html+='<!--优惠-->';
+				if(one.flag.toString()=="true"){
+					html+='<span class="discount">优惠</span>';
+					html+='<span class="triangle"></span>';
+					} 
+				html+='<!--优惠-->';
+				html+='<a href="'+one.url+'"><img src="'+one.imageUrl+'" alt="'+one.title+'"></a>';
+				html+='<span class="like"><i class="icon-heart icon-white"></i>'+one.likeCount+'+</span>';
+				html+='</div>';
+				html+='<h4><a href="'+one.url+'" title="">'+one.title+'</a></h4>';
+				html+='<small><span class="pull-left num">吊牌价：<em>￥'+one.originalPrice+'</em></span><span class="pull-right price">销售价：<em>￥'+one.price+'</em></span></small>';
+				html+='</div>';
+				html+='</li>';
+				var elem = $(html).get(0);
 				fragment.appendChild(elem);
-				elems.push( elem );
-			  }
-			
-				// Start Masonry
-				scrollContainer.imagesLoaded( function(){
-					scrollContainer.masonry({
-						itemSelector : '.scrollItem'
+				elems.push(elem);
+			});
+			_this._container.append(fragment);
+			_this._container.imagesLoaded(function() {
+				if (!_this._isMsnryInit) {
+					_this._isMsnryInit = true;
+					_this._container.masonry({
+						itemSelector: '.scrollItem'
 					});
-				});
+					_this._msnry = _this._container.data('masonry');
+				} else {
+					_this._msnry.appended(elems);
+				}
+			});
+		},
+		loadData: function($type, $entity_id) {
+			this._isLoadingMore = true;
+			$('#loaderCircle').show();
+			var _this = this;
+			type = $type;
+			entity_id = $entity_id;
+			$.ajax({
+				url: this.listUrl(),
+				dataType: 'jsonp',
+				data: {
+					page: this._page,
+					type: type,
+					entity_id: entity_id
+				},
+				success: this.onLoad
+			}).always(function() {
+				_this._isLoadingMore = false;
+				$('#loaderCircle').hide();
+			});
 
-				scrollContainer.append(fragment);
-				msnry.appended(elems);
-      };
-	   function get_source(){
-	   var str =$('#info').attr('info');
-	   var arr= new Array(); //定义一数组
-       arr=str.split(","); //字符分割   
-	   type = arr[0];
-	   entity_id = arr[1];
-	   }
-	   
-       $(document).ready(new function() {
-      // Capture scroll event.
-       $(document).bind('scroll', onScroll);
-       get_source();
-      // Load first data from the API.
-       loadData(type,entity_id);
-									  });
+		},
+		clears: function() {
+
+			this._page = 1;
+			this._container.empty();
+			if (this._msnry) {
+				var items = this._msnry.getItemElements();
+				if (items && items.length>0) {
+					this._msnry.remove(items);	
+					this._isMsnryInit= false;
+					this._msnry.destroy();
+				}
+			}
+		},
+		onScroll: function(event) {
+			// Only check when we're not still waiting for data.
+			var _this = intime.index;
+			if (!_this._isLoadingMore) {
+				// Check if we're within 100 pixels of the bottom edge of the broser window.
+				var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
+				if (closeToBottom) {
+					_this.loadData(type, entity_id);
+				}
+			}
+		},
+		listUrl: function() {
+			return intime.env.host + this._listpath;
+		}
+	}
+});
