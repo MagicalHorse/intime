e8b9f290e9adee562578d70cda1e5c21580f6fff
hanqing <hanqing@rd.tuan800.com>
2013-11-18 21:57:01 +0800

    fix bug

--- public/js/space.js
+++ public/js/space.js
@@ -212,27 +212,27 @@ $.extend(intime, {
 					html += '<li class="scrollItem">';
 					html += '<div class="thumbnail">';
 					html += '<div class="action">';
-					html += '<a href="product.html"><img src="' + one.imageUrl + '" alt="' + one.imageUrl + '"></a>';
+					html += '<a href="' + one.url + '"><img src="' + one.imageUrl + '" alt="' + one.imageUrl + '"></a>';
 					html += '<span class="like"><i class="icon-heart icon-white"></i>' + one.likeCount + '+</span>';
 					html += '</div>';
-					html += '<h4><a href="product.html">' + one.title + '</a></h4>';
+					html += '<h4><a href="' + one.url + '">' + one.title + '</a></h4>';
 					html += '<small><span class="pull-left num">吊牌价：<em>￥' + one.originalPrice + '</em></span><span class="pull-right price">销售价：<em>￥' + one.price + '</em></span></small>';
 					html += '</div>';
 					html += '</li>';
 				} else if (loveType == 2) {
 					html += '<li class="scrollItem">';
 					html += '<div class="thumbnail">';
-					html += '<div class="action"> <a href="promo.html" title="' + one.title + '"><img src="' + one.imageUrl + '" alt="' + one.title + '"></a> </div>';
+					html += '<div class="action"> <a href="' + one.url + '" title="' + one.title + '"><img src="' + one.imageUrl + '" alt="' + one.title + '"></a> </div>';
 					html += '</div>';
 					html += '</li>';
 				} else if (loveType == 3) {
 					html += '<li class="scrollItem">';
 					html += '<div class="thumbnail">';
 					html += '<div class="action">';
-					html += '<a href="product.html"><img src="' + one.imageUrl + '" alt="' + one.imageUrl + '"></a>';
+					html += '<a href="' + one.url + '"><img src="' + one.imageUrl + '" alt="' + one.imageUrl + '"></a>';
 					html += '<span class="like"><i class="icon-heart icon-white"></i>' + one.likeCount + '+</span>';
 					html += '</div>';
-					html += '<h4><a href="product.html">' + one.title + '</a></h4>';
+					html += '<h4><a href="' + one.url + '">' + one.title + '</a></h4>';
 					html += '<small><span class="pull-left num">吊牌价：<em>￥' + one.originalPrice + '</em></span><span class="pull-right price">销售价：<em>￥' + one.price + '</em></span></small>';
 					html += '</div>';
 					html += '</li>';
