5dc19b016743d67af59b744d04b2c4ef064f0289
tumayun <tumayun.2010@gmail.com>
2013-11-19 00:42:46 +0800

    update blank prompt

--- public/js/index_water.js
+++ public/js/index_water.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //首页广告图
 $('#banner').royalSlider({
 	//arrowsNav: false,是否用箭头导航,默认true
@@ -144,25 +145,39 @@ function clears(){
           success: onLoadData
         });
       };
+=======
+window.intime = window.intime || {};
+intime = window.intime;
+$.extend(intime, {
+	index: {
+		_page: 1,
+		_sort: '',
+		_listpath: 'front/promotions/get_list.json',
+		_container: $('#tiles'),
+		_msnry: null,
+		_isMsnryInit: false,
+		_isLoadingMore: false,
+>>>>>>> update blank prompt
 
-      /**
-       * Receives data from the API, creates HTML for images and updates the layout
-       */
-      function onLoadData(data) {
-        _isLoadingMore = false;
-        $('#loaderCircle').hide();
+		onLoad: function(data) {
+			var _this = intime.index;
+			var length = data.datas.length;
+			if (_this._page == 1) {
+				if (length <= 0) {
+					$('#no_data').show();
+					return;
+				}
+			} else {
+				if (length <= 0) {
+					$('#last_page').show();
+					return;
+				}
+			}
+			_this._page++;
 
-        // Increment page index for future calls.
-        page++;
-
-          var i=0, length=data.datas.length;
-			var html = '';
-			if (length<=0)
-			    html = '<p style="text-align:center;font-size: 16px;line-height:30px;">更多精彩活动敬请期待！</p>';
-				$('#tiles').after(html);
-				//alert(length);
 			var elems = [];
 			var fragment = document.createDocumentFragment();
+<<<<<<< HEAD
 
 
 			  for ( ; i < length; i++ ) {
@@ -343,16 +358,93 @@ function clears(){
 					scrollContainer.masonry({
 						itemSelector : '.scrollItem'
 					});
+=======
+			if (sort == 1) {
+				$(data.datas).each(function() {
+					var html = '';
+					var one = this;
+					html += '<li class="scrollItem">';
+					html += '<div class="thumbnail">';
+					html += '<h3><i class="icon_title"></i><a href="' + one.url + '" title="">' + one.title + '</a></h3>';
+					html += '<div class="action"> <a href="promo.html"><img src="' + one.imageUrl + '" alt=" "></a>';
+					html += '<p>喜欢银泰，乐享三倍积点。银泰年中庆，小积点也能玩出大动作，三倍积点大赠送啦！</p>';
+					html += '</div>';
+					html += '<h3 class="time bottom">活动时间：<span>' + one.startDate + '-' + one.endDate + '</span></h3>';
+					html += '<small> <span class="pull-left"><a href="shop.html"><i class="icon-map-marker"></i>' + one.storeName + '</a></span> <span class="pull-right"><i class="icon-heart"></i>' + one.likeCount + '+</span> </small> </div>';
+					html += '</li>';
+					var elem = $(html).get(0);
+					fragment.appendChild(elem);
+					elems.push(elem);
 				});
+			} else if (sort == 2) {
+				$(data.datas).each(function() {
+					var html = '';
+					var one = this;
+					html += '<li class="scrollItem">';
+					html += '<div class="thumbnail">';
+					html += '<h3 class="time">活动时间：<span>' + one.startDate + '-' + one.endDate + '</span></h3>';
+					html += '<div class="action"> <a href="#"><img src="' + one.imageUrl + '" alt=" "></a>';
+					html += '<p>喜欢银泰，乐享三倍积点。银泰年中庆，小积点也能玩出大动作，三倍积点大赠送啦！</p>';
+					html += '</div>';
+					html += '<h3 class="bottom"><i class="icon_title"></i><a href="' + one.url + '" title="">' + one.title + '</a></h3>';
+					html += '<small> <span class="pull-left"><a href="shop.html"><i class="icon-map-marker"></i>' + one.storeName + '</a></span> <span class="pull-right"><i class="icon-heart"></i>' + one.likeCount + '+</span> </small> </div>';
+					html += '</li>';
+					var elem = $(html).get(0);
+					fragment.appendChild(elem);
+					elems.push(elem);
+>>>>>>> update blank prompt
+				});
+			} else if (sort == 3) {
+				$(data.datas).each(function() {
+					var html = '';
+					var one = this;
+					html += '<li class="scrollItem">';
+					html += '<div class="thumbnail">';
+					html += '<h3><i class="icon_title"></i><a href="' + one.url + '" title="">' + one.title + '</a></h3>';
+					html += '<div class="action"> <a href="promo.html"><img src="' + one.imageUrl + '" alt=" "></a>';
+					html += '<p>喜欢银泰，乐享三倍积点。银泰年中庆，小积点也能玩出大动作，三倍积点大赠送啦！</p>';
+					html += '</div>';
+					html += '<h3 class="time bottom">活动时间：<span>' + one.startDate + '-' + one.endDate + '</span></h3>';
+					html += '<small> <span class="pull-left"><a href="shop.html"><i class="icon-map-marker"></i>' + one.storeName + '</a></span> <span class="pull-right"><i class="icon-heart"></i>' + one.likeCount + '+</span> </small> </div>';
+					html += '</li>';
+					var elem = $(html).get(0);
+					fragment.appendChild(elem);
+					elems.push(elem);
+				});
+			}
 
-				scrollContainer.append(fragment);
-				msnry.appended(elems);
-      };
-	   
-       $(document).ready(new function() {
-      // Capture scroll event.
-      $(document).bind('scroll', onScroll);
+			_this._container.append(fragment);
+			_this._container.imagesLoaded(function() {
+				if (!_this._isMsnryInit) {
+					_this._isMsnryInit = true;
+					_this._msnry = new Masonry(_this._container[0], {
+						itemSelector: '.scrollItem'
+					});
+				} else {
+					_this._msnry.appended(elems);
+				}
+			});
+		},
+		loadData: function($sort) {
+			this._isLoadingMore = true;
+			$('#loader').show();
+			$('#no_data,#last_page').hide();
+			var _this = this;
+			sort = $sort;
+			$.ajax({
+				url: this.listUrl(),
+				dataType: 'jsonp',
+				data: {
+					page: this._page,
+					sort: $sort
+				},
+				success: this.onLoad
+			}).always(function() {
+				_this._isLoadingMore = false;
+				$('#loader').hide();
+			});
 
+<<<<<<< HEAD
       // Load first data from the API.
       loadData('3');
 <<<<<<< HEAD
@@ -397,3 +489,35 @@ function clears(){
 =======
 									  });
 >>>>>>> 修改首页展示信息
+=======
+		},
+		clears: function() {
+			this._page = 1;
+			$('#no_data,#last_page').hide();
+			this._container.empty();
+			if (this._msnry) {
+				var items = this._msnry.getItemElements();
+				if (items && items.length > 0) {
+					this._msnry.remove(items);
+					this._isMsnryInit = false;
+					this._msnry.destroy();
+				}
+			}
+		},
+		onScroll: function(event) {
+			// Only check when we're not still waiting for data.
+			var _this = intime.index;
+			if (!_this._isLoadingMore) {
+				// Check if we're within 100 pixels of the bottom edge of the broser window.
+				var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
+				if (closeToBottom) {
+					_this.loadData(sort);
+				}
+			}
+		},
+		listUrl: function() {
+			return intime.env.host + this._listpath;
+		}
+	}
+});
+>>>>>>> update blank prompt
--- public/js/promo.js
+++ public/js/promo.js
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> update js and css
 //产品页面图片
@@ -25,34 +26,114 @@ function get_source(){
   sourcetype = strs[0];
   sourceid = strs[1];
 }
+=======
+window.intime = window.intime || {};
+intime = window.intime;
+$.extend(intime, {
+	index: {
+		_page: 1,
+		_sort: '',
+		_listpath: 'comment/get_list.json',
+		_container: $('#tiles'),
+		_msnry: null,
+		_isMsnryInit: false,
+		_isLoadingMore: false,
+>>>>>>> update blank prompt
 
-var handler = null;
-var page = 1;
-var isLoading = false;
-var apiURL = 'http://stage.youhuiin.com/front/comments/get_list.json'
+		onLoad: function(data) {
+			var _this = intime.index;
+			var length = data.datas.length;
+			if (_this._page == 1) {
+				if (length <= 0) {
+					$('#no_data').show();
+					return;
+				}
+			} else {
+				if (length <= 0) {
+					$('#last_page').show();
+					return;
+				}
+			}
+			_this._page++;
+			var num = data.totalcount;
+	        $('#totalcount').html('('+num+')');
+			if (length <= 0) return;
+			var elems = [];
+			var fragment = document.createDocumentFragment();
+			$(data.datas).each(function() {
+				var html = '';
+				var one = this;
+				leng = one.comments.length;
+				html += '<li class="post scrollItem">';
+				html += '<div class="post-self">';
+				html += '<div class="avatar"><a rel="nofollow author" href="#" title="' + one.customer.nickname + '"><img src="' + one.customer.logo + '" alt="' + one.customer.nickname + '"></a></div>';
+				html += '<div class="comment-body">';
+				html += '<div class="comment-header"><a class="user-name highlight" rel="nofollow" target="_blank">' + one.customer.nickname + '</a></div>';
+				html += '<p>' + one.content + '</p>';
+				html += '<div class="comment-footer comment-actions"><span class="time" datetime="' + one.createTime + '" title="' + one.createTime + '">' + one.createTime + ' - ' + one.floor + '楼</span><a class="post-reply" href="#reply" data-toggle="modal"><span class="icon icon-reply"></span>回复</a></div>';
+				html += '</div>';
+				html += '</div>';
+				var c = 0,
+				leng = one.comments.length;
+				if (leng != 0) {
+					for (; c < leng; c++) {
+						html += '<ul class="children">';
+						html += '<li class="post">';
+						html += '<div class="post-self">';
+						html += '<div class="avatar"><a rel="nofollow author" href="#" title="Lengxu"><img src="' + one.comments[c].customer.logo + '" alt="' + one.comments[c].customer.nickname + '"></a></div>';
+						html += '<div class="comment-body">';
+						html += '<div class="comment-header"><a class="user-name highlight" href="#" rel="nofollow" target="_blank">' + one.comments[c].customer.nickname + '</a></div>';
+						html += '<p>' + one.comments[c].content + '</p>';
+						html += '<div class="comment-footer comment-actions"><span class="time" datetime="' + one.comments[c].createTime + '" title="' + one.comments[c].createTime + '">' + one.comments[c].createTime + '</span><a class="post-reply" href="#reply" data-toggle="modal"><span class="icon icon-reply"></span>回复</a></div>';
+						html += '</div>';
+						html += '</div>';
+						html += '</li>';
+						html += '</ul>';
+					}
+				}
+				html += '</li>';
 
-// Prepare layout options.
-var options = {
-  autoResize: true, // This will auto-update the layout when the browser window is resized.
-  container: $('#tiles'), // Optional, used for some extra CSS styling
-  offset: 2, // Optional, the distance between grid items
-  itemWidth: 210 // Optional, the width of a grid item
-};
+				var elem = $(html).get(0);
+				fragment.appendChild(elem);
+				elems.push(elem);
+			});
+			_this._container.append(fragment);
+			_this._container.imagesLoaded(function() {
+				if (!_this._isMsnryInit) {
+					_this._isMsnryInit = true;
+					_this._msnry = new Masonry(_this._container[0], {
+						itemSelector: '.scrollItem'
+					});
+				} else {
+					_this._msnry.appended(elems);
+				}
+			});
+		},
+		loadData: function($sourceid,$sourcetype) {
+			this._isLoadingMore = true;
+			$('#loader').show();
+			$('#no_data,#last_page').hide();
+			var _this = this;
+			sourceid = $sourceid;
+			sourcetype = $sourcetype;
+			$.ajax({
+				url: this.listUrl(),
+				dataType: 'jsonp',
+				data: {
+					page: this._page,
+					sourceid: sourceid,
+					sourcetype: sourcetype
+				},
+				success: this.onLoad
+			}).always(function() {
+				_this._isLoadingMore = false;
+				$('#loader').hide();
+			});
 
-/**
- * When scrolled all the way to the bottom, add more tiles.
- */
-function onScroll(event) {
-  // Only check when we're not still waiting for data.
-  if(!isLoading) {
-    // Check if we're within 100 pixels of the bottom edge of the broser window.
-    var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
-    if(closeToBottom) {
-      loadData();
-    }
-  }
-};
+		},
+		clears: function() {
 
+<<<<<<< HEAD
 /**
  * Refreshes the layout.
  */
@@ -360,3 +441,32 @@ $(document).ready(new function() {
   loadData();
 });
 >>>>>>> 调试促销详情页评论
+=======
+			this._page = 1;
+			this._container.empty();
+			if (this._msnry) {
+				var items = this._msnry.getItemElements();
+				if (items && items.length > 0) {
+					this._msnry.remove(items);
+					this._isMsnryInit = false;
+					this._msnry.destroy();
+				}
+			}
+		},
+		onScroll: function(event) {
+			// Only check when we're not still waiting for data.
+			var _this = intime.index;
+			if (!_this._isLoadingMore) {
+				// Check if we're within 100 pixels of the bottom edge of the broser window.
+				var closeToBottom = ($(window).scrollTop() + $(window).height() > $(document).height() - 100);
+				if (closeToBottom) {
+					_this.loadData(sourceid,sourcetype);
+				}
+			}
+		},
+		listUrl: function() {
+			return intime.env.host + this._listpath;
+		}
+	}
+});
+>>>>>>> update blank prompt
