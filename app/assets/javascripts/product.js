374e40ff7890273f00e5eccfffbe966c91e4a373
hanqing <hanqing@rd.tuan800.com>
2013-11-25 22:45:25 +0800

    更新 js

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -5,12 +5,14 @@ $.extend(intime, {
 		
 		_page: 1,
 		_sort: '',
+
 		_listpath: 'comment/get_list.json',
 		_container: $('#tiles'),
 		_msnry: null,
 		_isMsnryInit: false,
 		_isLoadingMore: false,
 		_canLoadMore: false,
+
 
 		onLoad: function(data) {
 			
@@ -21,7 +23,6 @@ $.extend(intime, {
 					$('#no_data').show();
 					return;
 				}
-
 			} 
 			if (_this._page<data.totalpaged){
 				_this._canLoadMore = true;
@@ -29,6 +30,7 @@ $.extend(intime, {
 			} else {
 				_this._canLoadMore = false;
 				$('#last_page').show();
+
 			}
 			_this._page++;
 			var num = data.totalcount;
@@ -42,13 +44,14 @@ $.extend(intime, {
 				var html = '';
 				var one = this;
 				leng = one.comments.length;
-				html += '<li class="post scrollItem">';
+
+				html += '<li class="post scrollItem" id="reply_'+one.commentId+'">';
 				html += '<div class="post-self">';
-				html += '<div class="avatar"><a rel="nofollow author" href="#" title="' + one.customer.nickname + '"><img src="' + one.customer.logo + '" alt="' + one.customer.nickname + '"></a></div>';
+				html += '<div class="avatar"><a rel="nofollow author" href="'+one.customer.url+'" title="' + one.customer.nickname + '"><img src="' + one.customer.logo + '" alt="' + one.customer.nickname + '"></a></div>';
 				html += '<div class="comment-body">';
-				html += '<div class="comment-header"><a class="user-name highlight" rel="nofollow" target="_blank">' + one.customer.nickname + '</a></div>';
+				html += '<div class="comment-header"><a class="user-name highlight" href="'+one.customer.url+'" rel="nofollow" target="_blank">' + one.customer.nickname + '</a></div>';
 				html += '<p>' + one.content + '</p>';
-				html += '<div class="comment-footer comment-actions"><span class="time" datetime="' + one.createTime + '" title="' + one.createTime + '">' + one.createTime + ' - ' + one.floor + '楼</span><a class="post-reply" href="#reply" data-toggle="modal"><span class="icon icon-reply"></span>回复</a></div>';
+				html += '<div class="comment-footer comment-actions"><span class="time" datetime="' + one.createTime + '" title="' + one.createTime + '">' + one.createTime + '</span><a class="post-reply" href="#reply" data-toggle="modal" onclick="reply_comment(\''+one.commentId+'\', \''+one.customer.nickname+'\')"><span class="icon icon-reply"></span>回复</a></div>';
 				html += '</div>';
 				html += '</div>';
 				var c = 0,
@@ -58,11 +61,11 @@ $.extend(intime, {
 						html += '<ul class="children">';
 						html += '<li class="post">';
 						html += '<div class="post-self">';
-						html += '<div class="avatar"><a rel="nofollow author" href="#" title="Lengxu"><img src="' + one.comments[c].customer.logo + '" alt="' + one.comments[c].customer.nickname + '"></a></div>';
+						html += '<div class="avatar"><a rel="nofollow author" href="'+one.customer.url+'" title="Lengxu"><img src="' + one.comments[c].customer.logo + '" alt="' + one.comments[c].customer.nickname + '"></a></div>';
 						html += '<div class="comment-body">';
-						html += '<div class="comment-header"><a class="user-name highlight" href="#" rel="nofollow" target="_blank">' + one.comments[c].customer.nickname + '</a></div>';
+						html += '<div class="comment-header"><a class="user-name highlight" href="'+one.customer.url+'" rel="nofollow" target="_blank">' + one.comments[c].customer.nickname + '</a></div>';
 						html += '<p>' + one.comments[c].content + '</p>';
-						html += '<div class="comment-footer comment-actions"><span class="time" datetime="' + one.comments[c].createTime + '" title="' + one.comments[c].createTime + '">' + one.comments[c].createTime + '</span><a class="post-reply" href="#reply" data-toggle="modal"><span class="icon icon-reply"></span>回复</a></div>';
+						html += '<div class="comment-footer comment-actions"><span class="time" datetime="' + one.comments[c].createTime + '" title="' + one.comments[c].createTime + '">' + one.comments[c].createTime + '</span></div>';
 						html += '</div>';
 						html += '</div>';
 						html += '</li>';
@@ -141,4 +144,3 @@ $.extend(intime, {
 		}
 	}
 });
-
