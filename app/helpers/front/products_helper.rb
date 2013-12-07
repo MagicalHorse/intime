80918c44068088fe35e6b5748f35ec0d73ffca8d
Kimi <kimigao1986@gmail.com>
2013-12-07 22:25:29 +0800

    修改商品详情页中参与活动的图片展示方式

--- app/helpers/front/products_helper.rb
+++ app/helpers/front/products_helper.rb
@@ -5,4 +5,13 @@ module Front::ProductsHelper
   def audio_resource(p)
     @product.resource.select{|r| r[:type]==2}
   end
+
+  def display_promotion_image_url(promotion)
+    Settings.default_image_url.product.small
+    #if promotion.image_urls.present?
+      #promotion.image_urls.first
+    #else
+      #Settings.default_image_url.product.small
+    #end
+  end
 end
