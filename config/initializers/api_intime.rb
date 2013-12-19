7c84edb7200bc1c909c477c58c123cf770846f62
victor <gyyshuai@gmail.com>
2013-12-19 23:19:48 +0800

    update api host

--- config/initializers/api_intime.rb
+++ config/initializers/api_intime.rb
@@ -1,6 +1,8 @@
 RestClient.log = 'log/restclient.log'
 if Rails.env.production?
-  
+  API_HOST ="http://iapi.intime.com.cn/api"
+  CLIENT_VERSION = '3.0'
+  API_KEY = '4030D3A6-07FD-4BF4-93D8-564B950CC4A7'
 elsif Rails.env.stage?
   
 else
