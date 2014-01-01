6c15047a6e0dce78b57b89d4166f1e336f20a1d9
victor <gyyshuai@gmail.com>
2014-01-01 14:02:59 +0800

    fix invoice detail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,7 @@ IntimeService::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.action_controller.show_full_error_reports = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
