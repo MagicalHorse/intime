13a2054409a3bcb5288c1d798e7e84e6c92eca89
victor <gyyshuai@gmail.com>
2014-01-08 14:35:53 +0800

    fix cacheable

--- lib/cacheable.rb
+++ lib/cacheable.rb
@@ -1,17 +1,16 @@
 require 'dalli'
 module Cacheable
-  def cache_get(key,expires)
+  def cache_get(key,expires,&block)
     dc = nil;
     cache_item = nil;
     if Rails.env.production?
-      dc = Dalli::Client.new("#{Settings.elasticache.host}:#{Settings.elasticache.port}", { :namespace => "i.intime.com.cn", :compress => true })
-      cache_item = dc.get(key)
-    end
-    cache_item = yield if cache_item.nil?
-    return cache_item unless cache_item.nil?   
-    if Rails.env.production?
-      dc = Dalli::Client.new("#{Settings.elasticache.host}:#{Settings.elasticache.port}", { :namespace => "i.intime.com.cn", :compress => true }) if dc.nil?
-      dc.set(key,cache_item)
+      dc = Dalli::Client.new("#{Settings.elasticache.host}:#{Settings.elasticache.port}" \
+                    , { :namespace => "i.intime.com.cn", :compress => true ,:expires_in => expires})
+      cache_item = dc.fetch(key) do
+        block.call 
+      end
+    else
+      cache_item = block.call
     end
     cache_item
   end
