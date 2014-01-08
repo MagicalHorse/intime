7319cca5a40e0ef5c7402ef8503dc88e5138d788
victor <gyyshuai@gmail.com>
2014-01-08 11:40:29 +0800

    fix cache

--- lib/cacheable.rb
+++ lib/cacheable.rb
@@ -6,7 +6,7 @@ module Cacheable
       dc = Dalli::Client.new("#{Settings.elasticache.host}:#{Settings.elasticache.port}", { :namespace => "i.intime.com.cn", :compress => true })
       cache_item = dc.get(key)
     end
-    cache_item = yield  unless cache_item.nil?
+    cache_item = yield if cache_item.nil?
     return cache_item unless cache_item.nil?   
     if Rails.env.production?
       dc = Dalli::Client.new("#{Settings.elasticache.host}:#{Settings.elasticache.port}", { :namespace => "i.intime.com.cn", :compress => true }) if dc.nil?
