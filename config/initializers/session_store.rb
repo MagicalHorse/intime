6d8efce510c83ff61fe5c3e24fb8f706e23c02d1
victor <gyyshuai@gmail.com>
2014-03-04 15:06:22 +0800

    change session expire to 7 days

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-IntimeService::Application.config.session_store :cookie_store, key: '_IntimeService_session',:expire_after => 20.minutes
+IntimeService::Application.config.session_store :cookie_store, key: '_IntimeService_session',:expire_after => 7.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
