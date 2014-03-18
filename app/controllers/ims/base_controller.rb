cc3318f1e8a4bb57faf1565a742cc5b4a2095198
Ma Shuo <freebird0221@gmail.com>
2014-03-18 15:50:03 +0800

    auth

--- app/controllers/ims/base_controller.rb
+++ app/controllers/ims/base_controller.rb
@@ -2,8 +2,13 @@ class Ims::BaseController < ApplicationController
   before_filter :wx_auth!
   
   rescue_from Ims::Unauthorized do
-    redirect_to(URI::HTTPS.build([nil, "open.weixin.qq.com", nil, "/connect/oauth2/authorize", {appid: $weixin_appid, redirect_uri: URI.escape("http://#{request.host}/weixin/auth"), response_type: 'code', scope: 'snsapi_base', state: "STATE"}.to_param, 'wechat_redirect']).to_s)
-    cookies[:back_url] = request.url
+    redirect_to(URI::HTTPS.build([nil, "open.weixin.qq.com", nil, "/connect/oauth2/authorize", {appid: Settings.wx.appid, redirect_uri: URI.escape("http://#{Settings.wx.backdomain}/ims/auth"), response_type: 'code', scope: 'snsapi_base', state: "STATE"}.to_param, 'wechat_redirect']).to_s)
+    binding.pry
+    session[:back_url] = request.url
+  end
+  
+  def backurl
+    session[:back_url]||ims_cards_path
   end
   
   def wx_auth!
