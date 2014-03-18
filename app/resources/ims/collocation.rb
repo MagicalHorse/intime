ce5bb896ee736de1981bfeeb57944f1727fe3d7c
hupengxing <hupengxing@gmail.com>
2014-03-18 15:57:52 +0800

    add collocation

---
+++ app/resources/ims/collocation.rb
@@ -0,0 +1,20 @@
+class Ims::Collocation < Ims::Base
+
+  def add_product(product)
+
+  end
+
+  def remove_product(product)
+
+  end
+
+  def add_banner(banner)
+
+  end
+
+  def remove_banner(banner)
+
+  end
+
+
+end
