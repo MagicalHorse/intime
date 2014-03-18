556c1aceabb3a0426ba3eb542c6c5102cd855c88
Joey wang <wxluckly@gmail.com>
2014-03-18 10:24:07 +0800

    修改本地调试地址和环境

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -9,10 +9,10 @@ if Rails.env.production?
   AUDIO_DOMAIN = 'http://irss.ytrss.com/fileupload/audio/' 
 elsif Rails.env.stage?
   ES_DEFAULT_INDEX = 'intime'
-  PIC_DOMAIN = 'http://apis.youhuiin.com/fileupload/img/'
-  AUDIO_DOMAIN = 'http://apis.youhuiin.com/fileupload/audio/' 
+  PIC_DOMAIN = 'http://111.207.166.195/fileupload/img/'
+  AUDIO_DOMAIN = 'http://111.207.166.195/fileupload/audio/' 
 else
   ES_DEFAULT_INDEX = 'intime'
-  PIC_DOMAIN = 'http://apis.youhuiin.com/fileupload/img/' 
-  AUDIO_DOMAIN = 'http://apis.youhuiin.com/fileupload/audio/' 
+  PIC_DOMAIN = 'http://111.207.166.195/fileupload/img/' 
+  AUDIO_DOMAIN = 'http://111.207.166.195/fileupload/audio/' 
 end
