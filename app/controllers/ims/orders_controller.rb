444c3754b518c11094fc44e35d804c2cf1ff53c2
Ma Shuo <freebird0221@gmail.com>
2014-03-20 15:28:24 +0800

    order val setting

--- app/controllers/ims/orders_controller.rb
+++ app/controllers/ims/orders_controller.rb
@@ -1,5 +1,14 @@
 class Ims::OrdersController < Ims::Base
   def new
-    
+    @timeStamp_val = Time.now.to_i
+    @nonceStr_val = ("a".."z").to_a.simple(9)
+    sign = {
+      appid: Setting.wx.appid,
+      url: "http://open.weixin.qq.com/",
+      timeStamp: @timeStamp_val,
+      nonceStr: @nonceStr_val,
+      accessToken: Ims::Weixin.access_token
+    }.to_param
+    @addrSign_val = Digest::SHA1.hexdigest(sign)
   end
 end
