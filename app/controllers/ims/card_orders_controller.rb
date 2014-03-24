525f225e9b9f6b5e811c0366f1b39cb84f1e27b3
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-03-24 16:17:48 +0800

    非store下添加前端验证

--- app/controllers/ims/card_orders_controller.rb
+++ app/controllers/ims/card_orders_controller.rb
@@ -16,9 +16,9 @@ class Ims::CardOrdersController < Ims::BaseController
     @price = 500
     @code = 'AF135GBGQS'
   end
-  
+
   def paid
-    
+
   end
 private
   def pay
@@ -32,7 +32,7 @@ private
     # API_NEED: 对礼品卡付款之后，接收微信付款支付结果
     # 成功后进行订单状态修改等操作
     @package_val = {
-    	bank: "WX",	body: "商品描述",	partner: Setting.wx.partner_id,	out_trade_no: ,	total_fee: ,	fee_type: 1, 	notify_url: @notify_url,	spbill_create_ip: request.remote_ip,	time_start: Time.now.strftime('%Y%m%d%H%m%S'),	time_expire: 1.hours.from_now.strftime('%Y%m%d%H%m%S'),	transport_fee: ,	product_fee: ,	input_charset: 'UTF-8'
+    	# bank: "WX",	body: "商品描述",	partner: Setting.wx.partner_id,	out_trade_no: ,	total_fee: ,	fee_type: 1, 	notify_url: @notify_url,	spbill_create_ip: request.remote_ip,	time_start: Time.now.strftime('%Y%m%d%H%m%S'),	time_expire: 1.hours.from_now.strftime('%Y%m%d%H%m%S'),	transport_fee: ,	product_fee: ,	input_charset: 'UTF-8'
     }.to_param
     @paySign_val = Digest::SHA1.hexdigest({appid: Settings.wx.appid, timestamp: @timestamp_val, noncestr: @noncestr_val, package: @package_val, appkey: Settings.wx.appsecret}.to_params)
   end
