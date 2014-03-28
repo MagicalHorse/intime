23c1455e086425c92059c9d51065605ad84c6efe
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-03-28 11:31:53 +0800

    编辑更新product

--- app/controllers/ims/store/products_controller.rb
+++ app/controllers/ims/store/products_controller.rb
@@ -15,6 +15,11 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
   end
 
   def edit
+    @product = Ims::Product.find(request, {id: params[:id]})
+    @product = {id: 1, image: "/images/1.jpg", price: 100.1, brand_id: 2, brand_name: "mockup品牌1",
+      sales_code: "mockupsalescode1", sku_code: "sku_code", category_id: 1,
+      category_name: "mockup分类1", size_str: '1111', size: [{size_id: 1, size_name: "mockup尺码1"}, {size_id: 2, size_name: "mockup尺码2"}]}
+    @sizes = Ims::ProductSize.list(request, {category_id: @product[:category_id].to_i})
     product_relation_data
   end
 
@@ -38,12 +43,31 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
     end
   end
 
+  def update
+    product = Ims::Product.update(request, {
+      id: params[:id],
+      image: params["image"],
+      brand_id: params["brand_id"],
+      sales_code: params["coding_id"],
+      sku_code: params["item"],
+      price: params["weixin_price"],
+      category_id: params["category_id"],
+      size_str: params["size_str"],
+      size_ids: params["size_ids"]
+    })
+    if product["isSuccessful"]
+      redirect_to ims_store_products_path
+    else
+      redirect_to edit_ims_store_product_path(params[:id])
+    end
+  end
+
   def tutorials
   end
 
   def add_to_combo
     @combo_id = params[:combo_id]
-    product = Ims::Product.show(request, {:id => params[:id]})
+    product = Ims::Product.find(request, {:id => params[:id]})
     @combo.combo_products.create({:remote_id => product[:data][:id], :img_url => product[:data][:image], :product_type => "2", :price => product[:data][:price] })
     redirect_to new_ims_store_combo_path(:combo_id => @combo.id)
   end
