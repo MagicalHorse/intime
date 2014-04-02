b3347f205bd0dc03df722dde4174f28e86e75af4
hupengxing <hupengxing@gmail.com>
2014-04-02 15:57:35 +0800

    combos show change

--- app/controllers/ims/orders_controller.rb
+++ app/controllers/ims/orders_controller.rb
@@ -5,9 +5,9 @@ class Ims::OrdersController < Ims::BaseController
 
   def new
     @product = Ims::Product.find(request, id: params["id"])
-    @product = {id: 1, image: "/images/1.jpg", price: 100.1, brand_id: 2, brand_name: "mockup品牌1",
-      sales_code: "mockupsalescode1", sku_code: "sku_code", category_id: 1,
-      category_name: "mockup分类1", size_str: '1111', size: [{size_id: 1, size_name: "mockup尺码1"}, {size_id: 2, size_name: "mockup尺码2"}]}
+    # @product = {id: 1, image: "/images/1.jpg", price: 100.1, brand_id: 2, brand_name: "mockup品牌1",
+    #   sales_code: "mockupsalescode1", sku_code: "sku_code", category_id: 1,
+    #   category_name: "mockup分类1", size_str: '1111', size: [{size_id: 1, size_name: "mockup尺码1"}, {size_id: 2, size_name: "mockup尺码2"}]}
     @sizes = Ims::ProductSize.list(request, category_id: @product[:category_id])
     @timeStamp_val = Time.now.to_i
     @nonceStr_val = ("a".."z").to_a.sample(9).join('')
--- app/controllers/ims/store/base_controller.rb
+++ app/controllers/ims/store/base_controller.rb
@@ -2,8 +2,8 @@ class Ims::Store::BaseController < Ims::BaseController
   before_filter :authenticate
   layout "store"
 
-  #检查是否是店铺主人
+  #检查是否是店铺导购
   def authenticate
-
+    redirect_to ims_store_root_path if current_user.level != 3
   end
 end
