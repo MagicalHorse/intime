4a080c8d57ad7c584fee082ab4655a3c7a58da74
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-04-03 17:14:23 +0800

    已拍商品改回了接口调用

--- app/controllers/ims/store/products_controller.rb
+++ app/controllers/ims/store/products_controller.rb
@@ -2,8 +2,10 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
 
   def index
     @combo = ::Combo.find(params[:combo_id]) if params[:combo_id].present?
-    @search = ::Product.es_search(per_page: params[:per_page], page: params[:page])
-    @products = @search[:data]
+    # @search = ::Product.es_search(per_page: params[:per_page], page: params[:page])
+    # @products = @search[:data]
+    @search = Ims::Product.list(request, page: params[:page], pagesize: params[:per_page])
+    @products = @search["data"]["items"]
     respond_to do |format|
       format.html{}
       format.json{render "list"}
@@ -73,7 +75,6 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
   def add_to_combo
     @combo = ::Combo.find(params[:combo_id])
     product = Ims::Product.find(request, {:id => params[:id]})
-    binding.pry
     ComboProduct.create({:remote_id => product[:data][:id], :img_url => product[:data][:image], :product_type => "2", :price => product[:data][:price], :combo_id => @combo.id})
     redirect_to new_ims_store_combo_path(:combo_id => @combo.id)
   end
