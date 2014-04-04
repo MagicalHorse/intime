9f33d3e518953db7afb2d2ec6258845924bed28c
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-04-04 09:31:25 +0800

    商品管理套了简单的页面

--- app/controllers/ims/store/sells_controller.rb
+++ app/controllers/ims/store/sells_controller.rb
@@ -1,7 +1,7 @@
 class Ims::Store::SellsController < Ims::Store::BaseController
 
   def index
-    @gift_cards = Ims::Giftcard.all(request, pagesize: 10)["data"]["items"]
+    @gift_cards = Ims::Giftcard.all(request, pagesize: 5)["data"]["items"]
     @combos = Ims::Combo.list(request)["data"]["items"]
   end
 
