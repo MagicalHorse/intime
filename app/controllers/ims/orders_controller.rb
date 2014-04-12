0c7e42d919d2a6f6e4b855e7095461fa8eae1a6c
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-04-12 14:09:07 +0800

    fixbug

--- app/controllers/ims/orders_controller.rb
+++ app/controllers/ims/orders_controller.rb
@@ -51,6 +51,7 @@ class Ims::OrdersController < Ims::BaseController
 
   def payments
     @orderno = params[:id]
+    @from_page = params[:from_page]
     price = 0.01
     # @card_id, price = params[:money].split(",")
     #订单号 {子礼品卡编码}+{-}+{用户 id}+{-}+{来源店铺 id}
