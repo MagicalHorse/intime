0edb60d8c33f6ba62d537feeb9d467c2113a6d2e
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2014-05-01 06:08:04 +0800

    fixbug

--- app/controllers/ims/store/products_controller.rb
+++ app/controllers/ims/store/products_controller.rb
@@ -37,9 +37,10 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
     product = Ims::Product.create(request, {
       image_id: params["image_id"],
       brand_id: params["brand_id"],
-      sales_code: params["coding_id"],
+      sales_code: params["sales_code"],
       sku_code: params["sku_code"],
       price: params["price"],
+      unitprice: params["unitprice"],
       category_id: params["category_id"],
       size_str: params["size_str"],
       size_ids: params["size_ids"]
@@ -47,7 +48,7 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
 
     if product["isSuccessful"]
       ComboProduct.create({:remote_id => product[:data][:id], :img_url => product[:data][:image], :product_type => "2",
-       :price => product[:data][:price], :combo_id => @combo.try(:id),
+       :price => product[:data][:price], :unitprice => product[:data][:unitprice], :combo_id => @combo.try(:id),
        :brand_name => product[:data][:brand_name], :category_name => product[:data][:category_name]})
       redirect_to new_ims_store_combo_path(:combo_id => @combo.try(:id), t: Time.now.to_i)
     else
@@ -60,8 +61,9 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
       id: params[:id],
       image_id: params["image_id"],
       brand_id: params["brand_id"],
-      sales_code: params["coding_id"],
+      sales_code: params["sales_code"],
       sku_code: params["sku_code"],
+      unitprice: params["unitprice"],
       price: params["price"],
       category_id: params["category_id"],
       size_str: params["size_str"],
@@ -82,7 +84,7 @@ class Ims::Store::ProductsController < Ims::Store::BaseController
 
     product = params[:product_type] == "2" ? Ims::Product.find(request, {:id => params[:id]}) : ::Product.fetch_product(params[:id])
     ComboProduct.create({:remote_id => product[:data][:id], :img_url => product[:data][:image],
-      :product_type => params[:product_type], :price => product[:data][:price], :combo_id => @combo.id,
+      :product_type => params[:product_type], :price => product[:data][:price], :unitprice => product[:data][:unitprice], :combo_id => @combo.id,
       :brand_name => product[:data][:brand_name], :category_name => product[:data][:category_name]})
 
     redirect_to new_ims_store_combo_path(:combo_id => @combo.id, t: Time.now.to_i)
