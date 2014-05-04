75aa25c6219fe625a742bc62f1d7dcecc142d6ba
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-05-04 10:30:21 +0800

    店铺详情页面添加store_id

--- app/controllers/ims/stores_controller.rb
+++ app/controllers/ims/stores_controller.rb
@@ -1,11 +1,12 @@
 # encoding: utf-8
 class Ims::StoresController < Ims::BaseController
   layout "store"
-  
+
   def show
     @store = Ims::Store.show(request, id: params[:id])
     @title = "店铺展示"
     @can_share = true
     @template_id = @store[:data][:template_id]
+    session[:store_id] = @store[:data][:id]
   end
 end
