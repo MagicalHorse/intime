477f8347b245f1c3d1cb9e890731096f79297025
zhangcaiyan <zhangcaiyan@cn.jobs>
2014-06-23 13:04:27 +0800

    地址添加数量限制

--- app/controllers/ims/addresses_controller.rb
+++ app/controllers/ims/addresses_controller.rb
@@ -5,14 +5,15 @@ class Ims::AddressesController < Ims::BaseController
   before_filter :provinces, only:[:new, :edit]
 
   def index
-    @search = API::Address.index(request, page: params[:page], pagesize: params[:per_page] || 10)
+    # @search = API::Address.index(request, page: params[:page], pagesize: params[:per_page] || 10)
+    @search = API::Address.index(request)
     @addresses = @search["data"]["items"]
     @redirect_url = params[:redirect_url]
     @title = "我的地址"
-    respond_to do |format|
-      format.html{}
-      format.json{render "list"}
-    end
+    # respond_to do |format|
+    #   format.html{}
+    #   format.json{render "list"}
+    # end
   end
 
   def new
