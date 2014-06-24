9fb1807c03db0f2fb0d870c9c54b6a95930ead6b
Joey wang <wxluckly@gmail.com>
2014-06-24 10:25:37 +0800

    迷你银alipay 礼品卡

--- lib/alipay/lib/alipay/sign.rb
+++ lib/alipay/lib/alipay/sign.rb
@@ -1,20 +1,19 @@
 # encoding: utf-8
 module Alipay
   module Sign
-    def self.generate(params)
+    def self.generate(params, md5_key = nil)
       query = params.sort.map do |key, value|
         "#{key}=#{value}"
       end.join('&')
-
-      Digest::MD5.hexdigest("#{query}#{Alipay.key}")
+      Digest::MD5.hexdigest("#{query}#{md5_key || Alipay.key}")
     end
 
-    def self.verify?(params)
+    def self.verify?(params, md5_key = nil)
       params = Utils.stringify_keys(params)
       params.delete('sign_type')
       sign = params.delete('sign')
 
-      generate(params) == sign
+      generate(params, md5_key) == sign
     end
   end
 end
